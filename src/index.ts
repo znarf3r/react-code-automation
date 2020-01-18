41c8d716aede30e0b685c46268cea79ff15b4d21
Franz Recinos <flamana@gmail.com>
2020-01-18 14:59:49 -0500

    Refactor: first working config

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// import chalk fr"chalk"lk';
+import clear from 'clear';
+import figlet from 'figlet';
+// import p"commander" 'commander';
+// import arg = process.argv[2];
+clear();
+console.log('WOW this setup works!');
+// figlet.fonts(({err, fonts}) => {
+//   if (err) {
+//         console.log('something went wrong...');
+//         console.dir(err);
+//         return;
+//     }
+//     console.dir(fonts);
+// })
+// console.log(
+//   chalk.red(
+//     figlet.textSync('pizza-cli', { horizontalLayout: 'full' })
+//   )
+// );
